325db5af384c6520b9253a5bb4d6fb1036adf0d7
Erblin Krasniqi <erblini.kr@gmail.com>
2023-02-26 13:10:21 +0100

    About us page created

---
+++ aboutUs.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us | Our Watches</title>
+	<link rel="stylesheet" type="text/css" href="./css/about.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	<header>
+    <?php
+    include_once("header.php");
+
+    ?>
+	</header>
+
+	<main>
+		<h2>About Us</h2>
+		<p>Our Watches is a company that specializes in selling high-quality watches. We have a wide selection of watches that cater to different styles and preferences. Our watches are made with the finest materials and are designed to last for years.</p>
+
+		<p>Our company was founded in 2005 with the goal of providing our customers with the best watches at affordable prices. We believe that everyone should be able to own a high-quality watch without breaking the bank.</p>
+
+		<p>At Our Watches, we are committed to providing excellent customer service. We believe that our customers are our most valuable asset, and we always strive to exceed their expectations. We are passionate about watches, and we are always happy to share our knowledge and expertise with our customers.</p>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 Our Watches. All rights reserved.</p>
+	</footer>
+</body>
+</html>
